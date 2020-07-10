5ec8e28107f0f8d80ee5b0b65e65452232659af8
Lorewood <67896080+Lorewood@users.noreply.github.com>
2020-07-10 12:56:09 +0300

    Dwa

---
+++ Dwa/Models/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dwa.Models
+{
+    public class Data
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Midlename { get; set; }
+        public string Position { get; set; }
+    }
+}
---
+++ Dwa/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dwa.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Data> Users { get; set; }
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
